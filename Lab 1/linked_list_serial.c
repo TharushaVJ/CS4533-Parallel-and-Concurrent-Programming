19031dc82daa70cd12595c57d0d7b104d8131021
Tharusha_VJ <tharushaw.21@cse.mrt.ac.lk>
2025-09-03 13:10:28 +0530

    Base linked list structure and methods implemented

--- Lab 1/linked_list_serial.c
+++ Lab 1/linked_list_serial.c
@@ -1,1 +1,73 @@
-#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list_node_s{
+    int data;
+    struct list_node_s* next;
+};
+
+/* Member */
+int member(int value, struct list_node_s* head_p){
+    struct list_node_s* curr_p = head_p;
+
+    while(curr_p != NULL && curr_p->data < value){
+        curr_p = curr_p->next;
+    }
+
+    if(curr_p == NULL || curr_p->data > value){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+/* Insert */
+int insert(int value, struct list_node_s** head_pp){
+    struct list_node_s* curr_p = *head_pp;
+    struct list_node_s* pred_p = NULL;
+    struct list_node_s* temp_p = NULL;
+
+    while(curr_p != NULL && curr_p->data < value){
+        pred_p = curr_p;
+        curr_p = curr_p->next;
+    }
+
+    if(curr_p == NULL || curr_p->data > value){
+        temp_p = malloc(sizeof(struct list_node_s));
+        temp_p->data = value;
+        temp_p->next = curr_p;
+
+        if(pred_p == NULL){
+            *head_pp = temp_p;
+        } else {
+            pred_p->next = temp_p;
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/* Delete */
+int delete(int value, struct list_node_s** head_pp){
+    struct list_node_s* curr_p = *head_pp;
+    struct list_node_s* pred_p = NULL;
+    
+    while(curr_p != NULL && curr_p->data < value){
+        pred_p = curr_p;
+        curr_p = curr_p->next;
+    }
+
+    if(curr_p != NULL && curr_p->data == value){
+        if(pred_p == NULL){
+            *head_pp = curr_p->next;
+            free(curr_p);
+        } else {
+            pred_p->next = curr_p->next;
+            free(curr_p);
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+}
