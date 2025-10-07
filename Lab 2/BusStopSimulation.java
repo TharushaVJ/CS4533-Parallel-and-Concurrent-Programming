c29080178b57a55fa861d73ec85c58abc424ebdc
thejas0604 <thejas.meddepola@gmail.com>
2025-10-07 14:38:23 +0530

    feat: v1 solution

---
+++ Lab 2/BusStopSimulation.java
@@ -0,0 +1,90 @@
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+
+public class BusStopSimulation {
+    static final int BUS_CAPACITY = 50;
+    static final int MEAN_RIDER_ARRIVAL_MS = 3000; // 3 sec
+    // static final int MEAN_BUS_ARRIVAL_MS = 1200000; // 20 min
+    static final int MEAN_BUS_ARRIVAL_MS = 60000; // 1 min
+    static Semaphore mutex = new Semaphore(1);
+    static Semaphore busArrived = new Semaphore(0);
+    static Semaphore allAboard = new Semaphore(0);
+    static int waitingRiders = 0;
+    static int boardingRiders = 0;
+
+    public static void main(String[] args) {
+        // Start bus thread
+        new Thread(new Bus()).start();
+        // Start rider generator thread
+        new Thread(new RiderGenerator()).start();
+    }
+
+    static class RiderGenerator implements Runnable {
+        Random rand = new Random();
+        int riderId = 1;
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(getExpRandom(MEAN_RIDER_ARRIVAL_MS));
+                } catch (InterruptedException e) { }
+                new Thread(new Rider(riderId++)).start();
+            }
+        }
+        private int getExpRandom(int mean) {
+            return (int)(-Math.log(1 - rand.nextDouble()) * mean);
+        }
+    }
+
+    static class Bus implements Runnable {
+        Random rand = new Random();
+        int busId = 1;
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(getExpRandom(MEAN_BUS_ARRIVAL_MS));
+                } catch (InterruptedException e) { }
+                System.out.println("Bus " + busId + " arrived.");
+                mutex.acquireUninterruptibly();
+                int ridersToBoard = Math.min(waitingRiders, BUS_CAPACITY);
+                boardingRiders = ridersToBoard;
+                if (ridersToBoard > 0) {
+                    for (int i = 0; i < ridersToBoard; i++) {
+                        busArrived.release();
+                    }
+                    mutex.release();
+                    allAboard.acquireUninterruptibly();
+                } else {
+                    mutex.release();
+                }
+                System.out.println("Bus " + busId + " departed with " + ridersToBoard + " riders.");
+                busId++;
+            }
+        }
+        private int getExpRandom(int mean) {
+            return (int)(-Math.log(1 - rand.nextDouble()) * mean);
+        }
+    }
+
+    static class Rider implements Runnable {
+        int id;
+        public Rider(int id) { this.id = id; }
+        public void run() {
+            mutex.acquireUninterruptibly();
+            waitingRiders++;
+            System.out.println("Rider " + id + " arrived. Waiting riders: " + waitingRiders);
+            mutex.release();
+            busArrived.acquireUninterruptibly();
+            boardBus();
+        }
+        private void boardBus() {
+            mutex.acquireUninterruptibly();
+            waitingRiders--;
+            boardingRiders--;
+            System.out.println("Rider " + id + " boarded. Riders left to board: " + boardingRiders);
+            if (boardingRiders == 0) {
+                allAboard.release();
+            }
+            mutex.release();
+        }
+    }
+}
